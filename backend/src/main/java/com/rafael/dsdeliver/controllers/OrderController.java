838a8f0c9f9cbdb82665bc07e8a8444924874398
RafaelSouzaJava <rafael-souza4@outlook.com>
2021-01-04 13:01:27 -0300

    POST /orders

---
+++ backend/src/main/java/com/rafael/dsdeliver/controllers/OrderController.java
@@ -0,0 +1,40 @@
+package com.rafael.dsdeliver.controllers;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.rafael.dsdeliver.dto.OrderDTO;
+import com.rafael.dsdeliver.services.OrderService;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderController {
+
+	@Autowired
+	private OrderService orderService;
+	
+	@GetMapping
+	public ResponseEntity<List<OrderDTO>> findAll() {
+		List<OrderDTO> list =  orderService.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+		OrderDTO newDto = orderService.insert(dto);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(newDto.getId()).toUri(); 
+		
+		return ResponseEntity.created(uri).body(newDto);
+	}
+}
