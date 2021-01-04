611b5aa2606da9ee2a3e38a2b998c1f26bef4c0f
RafaelSouzaJava <rafael-souza4@outlook.com>
2021-01-04 11:14:44 -0300

    GET /products

---
+++ backend/src/main/java/com/rafael/dsdeliver/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.rafael.dsdeliver.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rafael.dsdeliver.entities.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
