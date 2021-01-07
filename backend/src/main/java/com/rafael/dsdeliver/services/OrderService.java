aadb731b265fe966637437f988af0cdf553b38a8
RafaelSouzaJava <rafael-souza4@outlook.com>
2021-01-07 11:56:52 -0300

    corrigindo erro ao criar repositorio front-web

---
+++ backend/src/main/java/com/rafael/dsdeliver/services/OrderService.java
@@ -0,0 +1,56 @@
+package com.rafael.dsdeliver.services;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.rafael.dsdeliver.dto.OrderDTO;
+import com.rafael.dsdeliver.dto.ProductDTO;
+import com.rafael.dsdeliver.entities.Order;
+import com.rafael.dsdeliver.entities.OrderStatus;
+import com.rafael.dsdeliver.entities.Product;
+import com.rafael.dsdeliver.repositories.OrderRepository;
+import com.rafael.dsdeliver.repositories.ProductRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> list = orderRepository.findOrdersWithProducts();	
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+				Instant.now(), OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId()); 
+			
+			order.getProducts().add(product);
+		}
+		order = orderRepository.save(order);
+		return new OrderDTO(order);
+	}
+	
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		Order order = orderRepository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = orderRepository.save(order);
+		return new OrderDTO(order);
+	}
+	
+
+}
