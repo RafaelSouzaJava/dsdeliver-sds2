88857e94aa96a606f7e860abb6123df6ed4164f3
RafaelSouzaJava <rafael-souza4@outlook.com>
2021-01-09 00:17:23 -0300

    Add env vars

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -2,7 +2,7 @@ import { OrderPayload } from './Orders/types';
 import axios from "axios";
 
 
-const API_URL = 'https://rafael-sds2.herokuapp.com';
+const API_URL = process.env.REACT_APP_API_URL;
 
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
